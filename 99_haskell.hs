e1a9e94ffaa87e0e348c5c6fd8f1e9b5a1731d90
elliotvilhelm <elliot@pourmand.com>
2019-03-21 14:54:12 -0700

    99

--- 99_haskell.hs
+++ 99_haskell.hs
@@ -13,3 +13,13 @@ elementAt (x:xs) n = elementAt xs (n-1)
 
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = xs == reverse xs
+
+-- P14 duplicate list
+duplicate :: [a] -> [a]
+duplicate [] = []
+duplicate (x:xs) = [x] ++ [x] ++ duplicate(xs)
+
+-- P15 replicate list
+repli :: [a] -> Int -> [a]
+repli [] n = []
+repli (x:xs) n = let replicated k = ((if k == 0 then [] else ([x] ++ (replicated (k-1))))) in (replicated n) ++ (repli xs n)
