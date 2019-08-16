26d273afdeb13aa317b0ef3c5dd4fafe827d4034
Elliot <elliot@pourmand.com>
2019-08-16 16:14:44 -0700

    scala 99

---
+++ 99_scala.scala
@@ -0,0 +1,87 @@
+object P1 {
+  // last element of a list
+  def run(): Unit = {
+    var res = P1.last(Array(1,2,3))
+    print(s"P1 (Last): ")
+    res.foreach(println)
+  }
+  def last(l: Array[Int]): Array[Int] = {
+    l match {
+      case Array(_) => l
+      case Array(_, _, _*)=> last(l.drop(1))
+    }
+  }
+}
+
+object P2 {
+  // second to last element of a list
+  def run(): Unit = {
+    var res = P2.secondLast(Array(1,2,3,4))
+    print(s"P2 (Second Last): ")
+    res.foreach(println)
+  }
+  def secondLast(l: Array[Int]): Array[Int] = {
+    l match {
+      case Array(x, _) => Array(x)
+      case Array(_, _, _, _*) => secondLast(l.drop(1))
+    }
+  }
+}
+
+object P3 {
+  // kth element of an array
+  def run(): Unit = {
+    var res = P3.kthElement(Array(0,1,2,3,4,5), 2)
+    println(s"P3 (Kth Element): $res")
+  }
+  def kthHelper(l: Array[Int], n: Int): Array[Int] = {
+    n match {
+      case 0 => l
+      case x => kthHelper(l.drop(1), n-1)
+    }
+
+  }
+  def kthElement(l: Array[Int], k: Int): Int = {
+    kthHelper(l, k)(0)
+  }
+}
+
+object P4 {
+  // length of array
+  def run(): Unit = {
+    var res = P4.getSize(Array(0,1,2,3,4,5))
+    println(s"P4 (Size): $res")
+  }
+  def getSize(l: Array[Int]): Int = {
+    l match {
+      case Array(_) => 1
+      case Array(_, _, _*) => getSize(l.drop(1)) + 1
+    }
+  }
+}
+
+object P5 {
+  def run(): Unit = {
+    var res = P5.reverse(Array(0,1,2,3,4,5))
+    print(s"P5 (Reverse): ")
+    res.foreach(print)
+    println()
+  }
+  def reverse(l: Array[Int]): Array[Int] = {
+    l match {
+      case Array(_) => l
+      case Array(a, _, _*) => reverse(l.drop(1)) ++ Array(a)
+    }
+  }
+
+}
+
+object Problems {
+  def main(args: Array[String]): Unit = {
+    P1.run()
+    P2.run()
+    P3.run()
+    P4.run()
+    P5.run()
+  }
+}
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# 99 Problems and I'm Functional
+# 99 Problems
+
+- Haskell
+- Prolog
+- Scala
 
 [H-99: Ninety-Nine Haskell Problems](https://wiki.haskell.org/H-99:_Ninety-Nine_Haskell_Problems)
 
 
-[H-99: Ninety-Nine Haskell Problems](https://sites.google.com/site/prologsite/prolog-problems)
+[H-99: Ninety-Nine Prolog Problems](https://sites.google.com/site/prologsite/prolog-problems)
+
+[H-99: Ninety-Nine Scala Problems](http://aperiodic.net/phil/scala/s-99/)
